4d72c31576b13af63242fd8565e59a76668fd236
abraham <abraham@flutterwavego.com>
2025-03-13 21:44:13 +0000

    update version

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import tools from "./registered-tools.js";
 // Create server instance.
 const server = new McpServer({
     name: "flutterwave",
-    version: "0.1.0",
+    version: "0.2.0",
 }, {
     capabilities: {
         tools: {},
